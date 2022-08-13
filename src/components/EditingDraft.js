bb16c6f470b108d7e93f0541181b129cd828c1f7
Paaaark <hifipark0801@gmail.com>
2022-08-13 12:11:11 -0400

    Editing form updated

--- src/components/EditingDraft.js
+++ src/components/EditingDraft.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
+import { Stack } from '@mui/system';
+import { Button } from '@mui/material';
+import styles from '../styles.css';
 
 const EditingDraft = ({ draft }) => {
   return (
-    <Grid container direction='column' alignItems='center' paddingLeft="10px">
-        <TextField fullWidth label="Title" variant="standard" defaultValue={draft.title}/>
+    <Grid container direction='column' alignItems='center' paddingLeft='10px' paddingTop="10px">
+        <Grid container width="100vw" maxWidth="1000px" bgcolor='white' direction='column' alignItems='center'>
+          <div className='textBox'>
+            <Stack spacing={2} direction='column'>
+              <TextField fullWidth label="Title" variant="standard" defaultValue={draft.title}/>
+              <TextField fullWidth label="Description" variant="standard" defaultValue={draft.description}/>
+              <h2>Recipes</h2>
+              <Button color="secondary" variant="outlined">Add steps</Button>
+            </Stack>
+          </div>
+        </Grid>
     </Grid>
   )
 }

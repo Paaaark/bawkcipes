b797cbb8bb18bd7d292ea1bb69f6e4556c0c627c
Paaaark <hifipark0801@gmail.com>
2022-09-21 09:52:08 +0900

    Build folder included

---
+++ build/precache-manifest.74325cc90223fe4a626f65fa1ca9a193.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "429d0cb9839bd481b243",
+    "url": "/static/css/main.35d391c7.chunk.css"
+  },
+  {
+    "revision": "429d0cb9839bd481b243",
+    "url": "/static/js/main.429d0cb9.chunk.js"
+  },
+  {
+    "revision": "8bdf3a8b762da183e5fb",
+    "url": "/static/js/1.8bdf3a8b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1d1255f730793f1d32a50d10f5be0774",
+    "url": "/index.html"
+  }
+];
